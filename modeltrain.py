fc3e4056dd3203cbf41f452cba99ccfe30a04d43
sauravrana646 <42442467+sauravrana646@users.noreply.github.com>
2020-05-26 02:19:52 +0530

    Update modeltrain.py

--- modeltrain.py
+++ modeltrain.py
@@ -5,7 +5,8 @@ from keras.layers import Dense
 from keras.optimizers import RMSprop
 import keras.backend as k
 
-def addDense(num):
+#Function to add Dense Layers
+def addDense(num):           
     model.add(Dense(units=num,input_shape=(13,),
                 activation='relu',
                 kernel_initializer="he_normal"))
@@ -13,15 +14,16 @@ def addDense(num):
 
 
 
-
+# Read Dataset
 dataset = pd.read_csv("wines.csv")
 
-
+# To be Predicted is stored in y 
 y = dataset["Class"]
 
-
+# Removed Categorical data to avoid dummy trap
 y_new = pd.get_dummies(y)
 
+# Removed to be predicted data from input
 X = dataset.drop('Class', axis =1 )  
 
 
@@ -31,24 +33,26 @@ sc = StandardScaler()          # To get better accuracy we use scaling here
 X = sc.fit_transform(X)
 
 
-
+# Building model
 model = Sequential()
 
+# Adding layers
 addDense(5)
 
 
-
+# Adding output layer
 model.add(Dense(units=3, activation='softmax'))
 
-
+# Compliling the model
 model.compile(optimizer= RMSprop(learning_rate=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
 
-
+# Training the model and fitting the data
 TrainedModel = model.fit(X,y_new,epochs = 10)
+
 
 k.clear_session()
 
+# Get Accuracy
 accuracy = (TrainedModel.history['accuracy'][-1:][0])*100
-
 
 print(accuracy)
